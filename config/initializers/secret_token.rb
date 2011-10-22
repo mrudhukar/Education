fd1170352bf16e470ebe1c022842c2d52c7627ed
mrudhukar <mrudhukar@chronus.com>
2011-10-22 18:03:42 +0530

    Clean Rails Base App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Education::Application.config.secret_token = '9f0d8c52b9516e321ece4d70c24e6c14415cf22a6427cb8dd45ef0bfe3389fc4d0dc9e87bd89ad4b994990c8394b1a48d738632adb2ba53b091d5b9de9c8aee5'
